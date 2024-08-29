5852eb383aa5470607a868e81fb814c8425b1db1
Omar Estrada <omarestrada908@gmail.com>
2024-08-28 22:39:36 -0500

    Removed unused lines within App.tsx due to failing build

--- frontend/main/src/App.tsx
+++ frontend/main/src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import FileUpload from './FileUpload'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Parquet Reader</h1>
