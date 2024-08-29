53815eeebced236c386ae3ca0978171400a6beb0
Omar Estrada <omarestrada908@gmail.com>
2024-08-29 01:05:25 -0500

    Updated main.py to use Flask for API endpoints and created two endpoints. One for sample data, and one to upload a file and return its content as JSON. Updated Dockerfile to use run flask for python file

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,68 @@
+import os
+import logging
 import duckdb
-import pandas as pd
+# import pandas as pd
+from flask import Flask, jsonify, request
+from werkzeug.utils import secure_filename
 
-# Connect to DuckDB
-con = duckdb.connect()
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = {'parquet'}
 
-# Query Parquet file and convert result to DataFrame
-df = con.execute("SELECT * FROM read_parquet('./test-data/sample1.parquet')").df()
+# Create instance of Flask
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Convert DataFrame to JSON-compatible format (dictionary)
-json_data = df.to_dict(orient='records')
+# Basic logging configuration
+logging.basicConfig(level=logging.DEBUG,  # Set the logging level to DEBUG
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
-# Test with print
-print(df)
-print(json_data)
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/file/parquet/sample", methods=['GET'])
+def get_sample_data():
+    # Connect to DuckDB
+    con = duckdb.connect()
+
+    # Query Parquet file and convert result to DataFrame
+    df = con.execute("SELECT * FROM read_parquet('./test-data/sample1.parquet')").df()
+
+    # Convert DataFrame to JSON-compatible format (dictionary) -> to JSON
+    return jsonify(df.to_dict(orient='records'))
+
+@app.route("/file/parquet", methods=['POST'])
+def convert_parquet_to_json():
+    logger.debug("Handling POST request for /file/parquet")
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    file = request.files['file']
+
+    logger.debug(f"Validating file.filename: {file.filename}")
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        if not os.path.exists(app.config['UPLOAD_FOLDER']):
+            logger.debug("Creating Directory: " + app.config['UPLOAD_FOLDER'])
+            os.makedirs(app.config['UPLOAD_FOLDER'])
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        logger.debug(f"Saving File to: {file_path}")
+        file.save(file_path)
+
+        # Connect to DuckDB
+        con = duckdb.connect()
+
+        # Query Parquet file and convert result to DataFrame
+        select_query = f"SELECT * FROM read_parquet('{file_path}')"
+        logger.debug(f'select_query to execute: {select_query}')
+        df = con.execute(select_query).df()
+
+        # Convert DataFrame to JSON-compatible format (dictionary) -> to JSON
+        return jsonify(df.to_dict(orient='records'))
+
+    logger.error('Something failed during file processing')
+    return jsonify({'error': 'Oops, something failed... Sorry!'}), 500
