aac485a7c1c609bc5c0602a9c1145dd95e62ecc2
Omar Estrada <omarestrada908@gmail.com>
2024-09-13 00:00:50 -0500

    Refactored by extracting duplicate code in connect to DuckDB and extracting content for parquet file, and replaced in all endpoints where it was being used.

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,7 @@ from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = {'parquet'}
+NO_FILES_HAVE_BEEN_UPLOADED = 'No files have been yet uploaded'
 
 # Create instance of Flask
 app = Flask(__name__)
@@ -25,13 +26,13 @@ def allowed_file(filename):
 def get_uploaded_parquet_list():
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         logger.debug('Upload Directory does not exist, no files uploaded')
-        return jsonify({'error': 'No files have been yet uploaded'}), 404
+        return jsonify({'error': NO_FILES_HAVE_BEEN_UPLOADED}), 404
     
     try:
         list_of_files = os.listdir(app.config['UPLOAD_FOLDER'])
         logger.debug(f"List of files within {app.config['UPLOAD_FOLDER']}, are {list_of_files}, with a size of {len(list_of_files)}")
         if len(list_of_files) == 0:
-            return jsonify({'error': 'No files have been yet uploaded'}), 404
+            return jsonify({'error': NO_FILES_HAVE_BEEN_UPLOADED}), 404
         filtered_files = [fileName for fileName in list_of_files if allowed_file(fileName)]
         logger.debug(f"Filtered list {filtered_files}, with a size of {len(filtered_files)}")
         return jsonify(filtered_files)
@@ -44,7 +45,7 @@ def search_uploaded_parquet_files():
     query_filename = request.args.get('filename')
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         logger.debug('Upload Directory does not exist, no files uploaded')
-        return jsonify({'error': 'No files have been yet uploaded'}), 404
+        return jsonify({'error': NO_FILES_HAVE_BEEN_UPLOADED}), 404
 
     if trim_and_check_if_blank(query_filename):
         logger.debug(f'Empty filename query parameter: {query_filename}')
@@ -61,37 +62,20 @@ def search_uploaded_parquet_files():
         if len(filtered_files) == 0:
             return jsonify({'error': 'No matching files found'}), 404
 
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], query_filename)
-        # Connect to DuckDB
-        con = duckdb.connect()
-
-        # Query Parquet file and convert result to DataFrame
-        select_query = f"SELECT * FROM read_parquet('{file_path}')"
-        logger.debug(f'select_query to execute: {select_query}')
-        df = con.execute(select_query).df()
-
-        # Convert DataFrame to JSON-compatible format (dictionary) -> to JSON
-        return jsonify(df.to_dict(orient='records'))
+        return get_parquet_file_content(query_filename, app.config['UPLOAD_FOLDER'])
     except Exception as e:
         logger.error(f"Error accessing files: {str(e)}")
         return jsonify({'error': 'Unable to access files'}), 500
 
 @app.route("/file/parquet/sample", methods=['GET'])
 def get_sample_data():
-    # Connect to DuckDB
-    con = duckdb.connect()
-
-    # Query Parquet file and convert result to DataFrame
-    df = con.execute("SELECT * FROM read_parquet('./test-data/sample1.parquet')").df()
-
-    # Convert DataFrame to JSON-compatible format (dictionary) -> to JSON
-    return jsonify(df.to_dict(orient='records'))
+    return get_parquet_file_content('sample1.parquet', './test-data')
 
 @app.route("/file/parquet", methods=['POST'])
 def convert_parquet_to_json():
     logger.debug("Handling POST request for /file/parquet")
     if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
+        return jsonify({'error': 'No file attached to request'}), 400
 
     file = request.files['file']
 
@@ -108,16 +92,7 @@ def convert_parquet_to_json():
         logger.debug(f"Saving File to: {file_path}")
         file.save(file_path)
 
-        # Connect to DuckDB
-        con = duckdb.connect()
-
-        # Query Parquet file and convert result to DataFrame
-        select_query = f"SELECT * FROM read_parquet('{file_path}')"
-        logger.debug(f'select_query to execute: {select_query}')
-        df = con.execute(select_query).df()
-
-        # Convert DataFrame to JSON-compatible format (dictionary) -> to JSON
-        return jsonify(df.to_dict(orient='records'))
+        return get_parquet_file_content(filename, app.config['UPLOAD_FOLDER'])
 
     logger.error('Something failed during file processing')
     return jsonify({'error': 'Oops, something failed... Sorry!'}), 500
@@ -129,3 +104,19 @@ def trim_and_check_if_blank(string):
 
     trimmed_string = string.strip()
     return not trimmed_string
+
+def get_parquet_file_content(file_name, directory_path):
+    file_path = os.path.join(directory_path, file_name)
+
+    # Connect to DuckDB
+    con = duckdb.connect()
+
+    # Logging Query
+    select_query = f"SELECT * FROM read_parquet('{file_path}')"
+    logger.debug(f'select_query to execute: {select_query}')
+
+    # Query Parquet file and convert result to DataFrame
+    df = con.execute(select_query).df()
+
+    # Convert DataFrame to JSON-compatible format (dictionary) -> to JSON
+    return jsonify(df.to_dict(orient='records'))
