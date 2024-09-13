12a2daa5cadc720cf1136bb4b8ba46e4f248f271
Omar Estrada <omarestrada908@gmail.com>
2024-09-12 23:46:48 -0500

    Added new endpoint to load content of already uploaded files. Adjusted endpoint name for list of uploaded files.

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/file/parquet", methods=['GET'])
+@app.route("/file/parquet/list", methods=['GET'])
 def get_uploaded_parquet_list():
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         logger.debug('Upload Directory does not exist, no files uploaded')
@@ -35,6 +35,43 @@ def get_uploaded_parquet_list():
         filtered_files = [fileName for fileName in list_of_files if allowed_file(fileName)]
         logger.debug(f"Filtered list {filtered_files}, with a size of {len(filtered_files)}")
         return jsonify(filtered_files)
+    except Exception as e:
+        logger.error(f"Error accessing files: {str(e)}")
+        return jsonify({'error': 'Unable to access files'}), 500
+
+@app.route("/file/parquet", methods=['GET'])
+def search_uploaded_parquet_files():
+    query_filename = request.args.get('filename')
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        logger.debug('Upload Directory does not exist, no files uploaded')
+        return jsonify({'error': 'No files have been yet uploaded'}), 404
+
+    if trim_and_check_if_blank(query_filename):
+        logger.debug(f'Empty filename query parameter: {query_filename}')
+        return jsonify({'error': 'Empty or missing filename query parameter'}), 400
+
+    if not allowed_file(query_filename):
+        logger.debug(f'Invalid file type, filename: {query_filename}')
+        return jsonify({'error': f'Invalid File Type: {query_filename}, file type support: {ALLOWED_EXTENSIONS}'}), 400
+
+    try:
+        list_of_files = os.listdir(app.config['UPLOAD_FOLDER'])
+        filtered_files = [fileName for fileName in list_of_files if query_filename in fileName]
+        logger.debug(f"Files found: {filtered_files}, Lenght of list: {len(filtered_files)}")
+        if len(filtered_files) == 0:
+            return jsonify({'error': 'No matching files found'}), 404
+
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], query_filename)
+        # Connect to DuckDB
+        con = duckdb.connect()
+
+        # Query Parquet file and convert result to DataFrame
+        select_query = f"SELECT * FROM read_parquet('{file_path}')"
+        logger.debug(f'select_query to execute: {select_query}')
+        df = con.execute(select_query).df()
+
+        # Convert DataFrame to JSON-compatible format (dictionary) -> to JSON
+        return jsonify(df.to_dict(orient='records'))
     except Exception as e:
         logger.error(f"Error accessing files: {str(e)}")
         return jsonify({'error': 'Unable to access files'}), 500
@@ -84,3 +121,11 @@ def convert_parquet_to_json():
 
     logger.error('Something failed during file processing')
     return jsonify({'error': 'Oops, something failed... Sorry!'}), 500
+
+def trim_and_check_if_blank(string):
+    # Check if string is None
+    if string is None:
+        return True
+
+    trimmed_string = string.strip()
+    return not trimmed_string
