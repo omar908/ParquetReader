4ab6ac61576fe982d6e1b343a11f60e6573d7c0b
Omar Estrada <omarestrada908@gmail.com>
2024-09-19 02:04:41 -0500

    Updated docker-compose to work with current frontend and backend application. Updated docker-compose to pass build arg to override environment variable of frontend baseApi. Updated Dockerfile for frontend application to support the override value, or use a fallback of localhost:5000. Added proxy into Vite, however does not work when in production mode. Added default.conf for nginx to handle proxying to backend docker container when running docker compose. Updated frontend Dockerfile to copy default.conf to ngnix container. Deleted unused files and directory of db-data.

--- frontend/main/vite.config.ts
+++ frontend/main/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://backend:5000', // Target URL of the backend service in Docker Compose
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api prefix from the request path
+      },
+    },
+  },
+});
